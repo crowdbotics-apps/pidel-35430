d3f41b123d8786361d9d6ad94d2b71d0c9f69558
Crowdbotics <team@crowdbotics.com>
2022-05-25 15:44:27 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Login(models.Model):
+    "Generated Model"
+    userid = models.BigIntegerField()
+    password = models.BigIntegerField()
